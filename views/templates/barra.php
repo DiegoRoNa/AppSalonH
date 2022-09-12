7148d39e06ff8b400c2bb920b7f5cc1e4dc14777
Diego Rojas Nava <diegorn.10@hotmail.com>
2022-09-11 22:02:30 -0500

    uploads15

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,14 +1,14 @@
 <div class="barra">
     <p>Bienvenido <?=$nombre ?? '';?></p>
 
-    <a class="boton" href="/logout">Cerrar sesión</a>
+    <a class="boton" href="/appsalonH/logout">Cerrar sesión</a>
 </div>
 
 <?php if(isset($_SESSION['admin'])): ?>
     <div class="barra-servicios">
-        <a href="/admin" class="boton">Ver citas</a>
-        <a href="/servicios" class="boton">Ver servicios</a>
-        <a href="/servicios/crear" class="boton">Nuevo servicio</a>
+        <a href="/appsalonH/admin" class="boton">Ver citas</a>
+        <a href="/appsalonH/servicios" class="boton">Ver servicios</a>
+        <a href="/appsalonH/servicios/crear" class="boton">Nuevo servicio</a>
     </div>
 <?php endif; ?>
 
