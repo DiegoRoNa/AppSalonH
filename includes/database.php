c6d134d36b8c2df24138d60858e45b5c953745f5
Diego Rojas Nava <diegorn.10@hotmail.com>
2022-09-15 21:38:17 -0500

    commit 1

--- includes/database.php
+++ includes/database.php
@@ -1,9 +1,9 @@
 <?php
 
 //CONEXIÓN A LA BASE DE DATOS
-//$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
+$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
 //$db = mysqli_connect('localhost', 'root', '', 'app_salon');
-$db = mysqli_connect('localhost', 'diegoron_admin', 'passadmindb', 'diegoron_appsalon');
+//$db = mysqli_connect('localhost', 'diegoron_admin', 'passadmindb', 'diegoron_appsalon');
 
 
 if (!$db) {
@@ -11,4 +11,6 @@ if (!$db) {
     echo "errno de depuración: " . mysqli_connect_errno();
     echo "error de depuración: " . mysqli_connect_error();
     exit;
+}else{
+    echo 'conectado';
 }
